8f848b5fb67f6d7e183b536bcde867a53d328ce8
Your Name <nymmestic@gmail.com>
2025-04-24 23:13:11 +0530

    file renaming and Base Error and custom Error creation

---
+++ middlewares/error-handler.js
@@ -0,0 +1,1 @@
+const error = (err, req, res, next) => {};
---
+++ utils/errors/base-error.js
@@ -0,0 +1,9 @@
+class BaseError extends Error {
+  constructor(name, statusCode, message) {
+    super(message);
+    this.name = name;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = BaseError;
