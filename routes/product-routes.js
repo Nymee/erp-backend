026a24ffad7fd13f03f6dd36979c9019b3b973b6
Your Name <nymmestic@gmail.com>
2025-05-19 00:43:40 +0530

    validation in progress

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { productValidationSchema } = require("../validators/product.validator");
+const validate = require("../middlewares/validator");
 
-router.get("/");
+router.post("/products", validate(productValidationSchema));
