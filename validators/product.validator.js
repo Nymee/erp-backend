bfffb81cf062672765ce60265290434a67f6093b
Your Name <nymmestic@gmail.com>
2025-05-23 00:35:20 +0530

    product validation errors- some fixed, sales order schema in progress

--- validators/product.validator.js
+++ validators/product.validator.js
@@ -21,20 +21,14 @@ const createProductSchema = Joi.object({
 });
 
 const updateProductSchema = Joi.object({
-  product_id: objectIdValidator("productId").required(),
+  product_id: objectIdValidator("productId").required(), //this should be in params right
   name: Joi.string().required(),
   cost_price: Joi.number().required(),
   retail_margin: Joi.number().required(),
   min_margin: Joi.number().required(),
   max_margin: Joi.number().required(),
   discount: Joi.number().optional(),
-  margin_unit: Joi.when("discount", {
-    //if discount key is there regardless of value
-    is: Joi.exist(),
-    then: Joi.string().valid("per", "rup").required(),
-    otherwise: Joi.forbidden(),
-  }),
-  discount_price: Joi.number().required(),
+  margin_unit: Joi.string().required(),
   gst: Joi.number().optional(),
   cess: Joi.number().optional(),
   sales_price: Joi.number().required(),
--- validators/sales-order.validator.js
+++ validators/sales-order.validator.js
@@ -1,9 +1,23 @@
 const Joi = require("joi");
 const objectIdValidator = require("../utils/object-validator");
 
-const createSalesOrderSchema = Joi.object({
+const createSOESchema = Joi.object({
   client_id: objectIdValidator("clientId"),
   products: Joi.array().items(salesProductSchema).required(),
+  so_discount: Joi.number().optional(),
+  so_discount_type: Joi.number().optional(),
+  type: Joi.string().valid("order", "estimation"),
 });
 
-const salesProductSchema = Joi.object({});
+const updateSOE = Joi.object({
+  products: Joi.array().items(salesProductSchema).optional(),
+  so_discount: Joi.number().optional(),
+  so_discount_type: Joi.number().optional(),
+});
+
+const salesProductSchema = Joi.object({
+  retail_margin: Joi.number().required(),
+  retail_margin_type: Joi.string().required(),
+  discount: Joi.number().required(),
+  discount_type: Joi.string().required(),
+});
