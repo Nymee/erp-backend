fc9b6f46423707557c43bbc91a452ad1f6205b6a
Nymee <nymmestic@gmail.com>
2025-05-27 18:05:15 +0530

    create almost done, update in progress

--- controllers/sales-controller.js
+++ controllers/sales-controller.js
@@ -6,5 +6,21 @@ const createSales = async (req, res, next) => {
     const salesInfo = req.body;
     const salesProducts = req.body.products;
     const value = validateSales(salesProducts);
-  } catch (err) {}
+  } catch (err) {
+    next(err);
+  }
 };
+
+const updateSales = async(req, res, next)=>{
+  try{
+    const salesInfo = req.body;
+    const salesProducts = req.body.products;
+    const value = validateSales(salesProducts);
+
+    if(value){
+      
+    }
+
+
+  }
+}
--- services/sales-validation.service.js
+++ services/sales-validation.service.js
@@ -1,8 +1,43 @@
 const Product = require("../models/Product");
+const validateProduct = require("./product-validation.service");
 
-function validateSales(salesProducts) {
+async function validateSales(salesProducts) {
+  //you redeclared salesProducts
   const salesProductIds = salesProducts.map((p) => p.product_id);
-  const originalProducts = Product.find({ _id: { $in: salesProductIds } });
+  const originalProducts = await Product.find({
+    _id: { $in: salesProductIds }, //comparison by value not by reference
+  }); //you forgot await
+  let validatedProductArray = [];
+
+  if (originalProducts && originalProducts.length > 0) {
+    //you forgot to check length
+    const salesProductsMap = new Map();
+    for (const product of salesProducts) {
+      // you wrote in instead of of
+      salesProductsMap.set(String(product.product_id), product);
+    }
+
+    for (const original of originalProducts) {
+      let currentProd = salesProductsMap.get(String(original._id)); // get uses strict equality ===.reference of objectids wont be same unless we convert to string. since string is primitive no reference and only value matter in ===.
+      if (currentProd) {
+        const prodToValidate = {
+          ...original.toObject(),
+          ...currentProd,
+        };
+        try {
+          const result = validateProduct(prodToValidate, "sales");
+        } catch (err) {
+          throw new Error(
+            `Validation failed for product ${original._id}: ${err.message}`
+          );
+        }
+        if (result) {
+          validatedProductArray.push(result);
+        }
+      }
+    }
+  }
+  return validatedProductArray;
 }
 
 module.exports = validateSales;
