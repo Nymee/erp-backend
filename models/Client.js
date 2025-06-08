da05783b0894eec037ef32e86ae142bd3f66f078
Your Name <nymmestic@gmail.com>
2025-06-08 12:47:05 +0530

    client model done with minimal fields

---
+++ models/Client.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const clientSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email_id: { type: String, required: true, unique: true },
+  mobile: { type: String, required: true, unique: true },
+  address: { type: String, required: true, unique: true },
+});
+
+const Client = mongoose.model("Client", clientSchema);
+
+module.exports = Company;
