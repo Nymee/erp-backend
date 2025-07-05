8596a73a100a36c18f3802ca9177dd1d3632cfa5
Nymee <nymmestic@gmail.com>
2025-07-05 12:30:48 +0530

    .

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.post(
   "/sign-up",
-  validate(createCompanySchema),
   authController.signupCompany
 );
 router.post("/login-in", verifyUser, authController.loginCompany);
